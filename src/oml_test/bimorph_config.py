54db3a779281f9b4e474288d37396fe34e37f980
Daniel Fernandes <danieljosephfernandes@gmail.com>
2025-02-19 15:08:25 +0000

    Add bimorph_config.py

---
+++ src/oml_test/bimorph_config.py
@@ -0,0 +1,14 @@
+from dodal.plans.bimorph import SlitDimension
+
+config = (
+    200.0,
+    SlitDimension.X,
+    0.0,
+    200.0,
+    0.05,
+    0.0,
+    0.05,
+    2,
+    0.0,
+    [0.0 for _ in range(8)],
+)
