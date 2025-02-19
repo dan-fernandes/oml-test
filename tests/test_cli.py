d06b5350cafb0d94525a65ce225838baed2b3c90
Daniel Fernandes <danieljosephfernandes@gmail.com>
2025-02-19 15:07:45 +0000

    Delete test_cli.py

--- tests/test_cli.py
+++
@@ -1,9 +0,0 @@
-import subprocess
-import sys
-
-from oml_test import __version__
-
-
-def test_cli_version():
-    cmd = [sys.executable, "-m", "oml_test", "--version"]
-    assert subprocess.check_output(cmd).decode().strip() == __version__
