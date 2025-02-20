b2e603f13241e0d7de379fdfd3dacc9ba7f27d25
Daniel Fernandes <danieljosephfernandes@gmail.com>
2025-02-20 16:28:17 +0000

    Refactor RE decleration out of count

--- src/oml_test/plans/count.py
+++ src/oml_test/plans/count.py
@@ -1,26 +1,12 @@
-import pathlib
-
 from bluesky.plans import count
-from bluesky.run_engine import RunEngine
 from dodal.beamlines import optics_metrology_lab
 from ophyd_async.core import init_devices
 
-from oml_test import csv_writer
-
-RE = RunEngine()
+from oml_test.runengine import RE
 
 with init_devices():
     mirror = optics_metrology_lab.mirror_one()
     slits = optics_metrology_lab.slits()
     detector = optics_metrology_lab.autocollimator()
-
-
-dir = pathlib.Path("/tmp/")
-
-path_provider = csv_writer.get_static_path_provider(dir)
-
-sub = csv_writer.csv_writer_subscription_builder(path_provider)
-
-RE.subscribe(sub)
 
 RE(count((mirror, slits, detector)))
