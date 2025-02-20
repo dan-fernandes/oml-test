f0402cc85c9090c6358e358b3731264cc3a8576e
Daniel Fernandes <danieljosephfernandes@gmail.com>
2025-02-20 16:29:05 +0000

    Refactor RE instantiation out of move_slits

--- src/oml_test/plans/move_slits.py
+++ src/oml_test/plans/move_slits.py
@@ -1,26 +1,11 @@
-import pathlib
-
 from bluesky.plans import scan
-from bluesky.run_engine import RunEngine
 from dodal.beamlines import optics_metrology_lab
 from ophyd_async.core import init_devices
 
-from oml_test import csv_writer
-
-RE = RunEngine()
+from oml_test.runengine import RE
 
 with init_devices():
-    mirror = optics_metrology_lab.mirror_one()
     slits = optics_metrology_lab.slits()
-    detector = optics_metrology_lab.autocollimator()
 
-
-dir = pathlib.Path("/tmp/")
-
-path_provider = csv_writer.get_static_path_provider(dir)
-
-sub = csv_writer.csv_writer_subscription_builder(path_provider)
-
-RE.subscribe(sub)
 
 RE(scan(slits, 0.0, 1.0))
